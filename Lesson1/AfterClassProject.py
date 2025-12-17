0ffdc200befa280de296e1e89e8c2d486b7c7f56
royroyalle <21ke21i3387@21kschool.school>
2025-12-17 09:24:36 +0530

    .

--- Lesson1/AfterClassProject.py
+++ Lesson1/AfterClassProject.py
@@ -1,4 +1,13 @@
-list = [1, 2, 3, 4, 5]
-count = 0
-for i in list:
-    count += i
+def squares(start, end):
+    sv = [e**2 for e in range(start, end + 1)]
+    even = [f for f in sv if f % 2 == 0]
+
+    odd = [f for f in sv if f % 2 != 0]
+    
+    print(f"Range: {start} to {end} ---")
+    print(f"All square values: {sv}")
+    print(f"Odd square values: {odd}")
+    print(f"Even square values: {even}")
+
+squares(2, 6)
+squares(1, 4)
